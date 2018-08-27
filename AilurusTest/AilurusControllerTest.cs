17f8ed49318c89d1fe6530ee38fe2f9193dfd4d3
yannickbattail <yannick.battail@gmail.com>
2018-08-28 00:55:08 +0200

    add AilurusControllerTest

---
+++ AilurusTest/AilurusControllerTest.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Ailurus.Controllers;
+using Ailurus.DTO.Implementation;
+using Xunit;
+using FluentAssertions;
+
+namespace AilurusTest
+{
+    public class AilurusControllerTest
+    {
+        [Fact]
+        public void SendInstructionsTest()
+        {
+            var controller = new AilurusController();
+            var instructions = new List<GlobalInstruction<CoordinateInt2D>>()
+            {
+                new GlobalInstruction<CoordinateInt2D>{
+                    TYPE = "Collect",
+                    DroneName = "Drone_1",
+                    Destination = null
+                },
+                new GlobalInstruction<CoordinateInt2D>{
+                    TYPE = "MoveTo",
+                    DroneName = "Drone_2",
+                    Destination = new CoordinateInt2D{
+                        X = 10,
+                        Y = 10
+                    }
+                }
+            };
+
+            var expected = new List<string>()
+            {
+                "",
+                ""
+            };
+            controller.GetPlayerContext();
+            var actual = controller.SendInstructions(instructions);
+            
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
