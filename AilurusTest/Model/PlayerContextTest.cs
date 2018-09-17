cb570bf1d8dd0a99ac42484a1e9e62ae033803a8
yannickbattail <yannick.battail@gmail.com>
2018-09-18 00:52:55 +0200

    improve unit tests of resource amount in player context

--- AilurusTest/Model/PlayerContextTest.cs
+++ AilurusTest/Model/PlayerContextTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Ailurus.DTO.Implementation;
 using Ailurus.Model;
 using Ailurus.Model.Instructions;
@@ -23,7 +24,7 @@ namespace AilurusTest.Model
         }
         
         [Fact]
-        public void GetStoredResourcesAt2Test()
+        public void GetStoredResourcesAtTest()
         {
             var initPos = new CoordinateInt2D() { X = 2, Y = 2};
             var rq = new ResourceQuantity() {Resource = ResourceType.Gold, Quantity = 42};
@@ -52,6 +53,55 @@ namespace AilurusTest.Model
 
             var actual = cntx.Resources;
             actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void GetStoredResourcesAt3Test()
+        {
+            var initPos = new CoordinateInt2D() { X = 2, Y = 2};
+            var rq = new ResourceQuantity() {Resource = ResourceType.Gold, Quantity = 42};
+            var mockedDrone = new Mock<IDrone<CoordinateInt2D>>();
+            mockedDrone
+                .Setup(dr => dr.GetValidInstructions())
+                .Returns(new List<IInstruction<CoordinateInt2D>>()
+                {
+                    new Unload<CoordinateInt2D>(new Drone<CoordinateInt2D>(initPos), new DateTime(2018,1,1,0,0,0))
+                    {
+                        Resource = new ResourceQuantity() {Resource = ResourceType.Gold, Quantity = 42}
+                    },
+                    new Unload<CoordinateInt2D>(new Drone<CoordinateInt2D>(initPos), new DateTime(2018,1,1,0,0,2))
+                    {
+                        Resource = new ResourceQuantity() {Resource = ResourceType.Silver, Quantity = 66}
+                    }
+                });
+            var mockedDrone2 = new Mock<IDrone<CoordinateInt2D>>();
+            mockedDrone2
+                .Setup(dr => dr.GetValidInstructions())
+                .Returns(new List<IInstruction<CoordinateInt2D>>()
+                {
+                    new Unload<CoordinateInt2D>(new Drone<CoordinateInt2D>(initPos), new DateTime(2018,1,1,0,0,0))
+                    {
+                        Resource = new ResourceQuantity() {Resource = ResourceType.Gold, Quantity = 42}
+                    }
+                });
+
+            var cntx = new PlayerContext<CoordinateInt2D>()
+            {
+                Drones = new List<IDrone<CoordinateInt2D>>()
+                {
+                    mockedDrone.Object,
+                    mockedDrone2.Object
+                }
+            };
+            IEnumerable<ResourceQuantity> expected = new List<ResourceQuantity>()
+            {
+                new ResourceQuantity() {Resource = ResourceType.Gold, Quantity = 84},
+                new ResourceQuantity() {Resource = ResourceType.Silver, Quantity = 66}
+            };
+
+            var actual = cntx.Resources;
+            var a = actual.ToList();
+            a.Should().BeEquivalentTo(expected);
         }
     }
 }
